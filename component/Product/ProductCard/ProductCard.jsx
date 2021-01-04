d1d96ce485b1fedade81faf48c5baa6585080470
fenyadim <dimos9813@google.com>
2021-01-04 21:03:27 +0800

    Поменял img на Image

--- component/Product/ProductCard/ProductCard.jsx
+++ component/Product/ProductCard/ProductCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 
 import styles from './ProductCard.module.scss';
 
@@ -8,7 +9,9 @@ export default function ProductCard({ title, price, img, gender }) {
       className={`${styles.productCard} ${
         gender === 'female' ? 'femaleTheme' : gender === 'male' ? 'maleTheme' : 'allTheme'
       }`}>
-      <img className={styles.productImg} src={img} alt={title} />
+      <div className={styles.productImg}>
+        <Image src={img} layout="fill" objectFit="contain" />
+      </div>
       <div className={styles.productInfo}>
         <h2 className={styles.productTitle}>{title}</h2>
         <span className={styles.productPrice}>{price} руб.</span>
