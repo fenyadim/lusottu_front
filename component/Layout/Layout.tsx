6fbfd245722cb1d4e8cf29a4ce689e36fa6df69b
fenyadim <dimos9813@google.com>
2021-01-06 20:10:23 +0800

    Поправил отображение в Head

--- component/Layout/Layout.tsx
+++ component/Layout/Layout.tsx
@@ -7,18 +7,31 @@ import styles from './Layout.module.scss';
 
 export default function Layout({ children }) {
   const router = useRouter();
-  const gender = router.query.gender;
+  const { gender, slug }: { gender?: string; slug?: string } = router.query;
+
+  function formattingSlug(slug: string) {
+    return (
+      slug &&
+      slug.replace(/-/g, ' ').replace(/(^|\s)\S/g, (a) => {
+        return a.toUpperCase();
+      })
+    );
+  }
+  function headTitle(gender: string, slug: string) {
+    if (gender === 'male') {
+      return 'Мужские духи - Lusottu';
+    } else if (gender === 'female') {
+      return 'Женский духи - Lusottu';
+    } else if (slug) {
+      return `${formattingSlug(slug)} - Lusottu`;
+    }
+    return 'Главная - Lusottu';
+  }
 
   return (
     <>
       <Head>
-        <title>
-          {gender === 'male'
-            ? 'Мужские духи - Lusottu'
-            : gender === 'female'
-            ? 'Женский духи - Lusottu'
-            : 'Главная - Lusottu'}
-        </title>
+        <title>{headTitle(gender, slug)}</title>
       </Head>
       <div
         className={`${styles.container} ${
