81c38e4f9fe13d2fe78fc4ada035449f552bfdc4
Dima <dimos9813@gmail.com>
2021-01-10 18:20:07 +0800

    Сделал меню

--- component/Search/Search.tsx
+++ component/Search/Search.tsx
@@ -9,67 +9,67 @@ import { SEARCH_ITEMS } from "../../lib/graphql/query";
 import styles from './Search.module.scss';
 
 export default function Search() {
-    const [value, setValue] = React.useState<string>('');
-    const [toggleContainer, setToggleContainer] = React.useState<boolean>(false);
-    const {data, loading} = useQuery(SEARCH_ITEMS, {variables: {search: value}, ssr: true});
-    const products: Array<ISearchProduct> = data?.products.nodes;
+  const [value, setValue] = React.useState<string>('');
+  const [toggleContainer, setToggleContainer] = React.useState<boolean>(false);
+  const {data, loading} = useQuery(SEARCH_ITEMS, {variables: {search: value}, ssr: true});
+  const products: Array<ISearchProduct> = data?.products.nodes;
 
-    React.useEffect(() => {
-        setValue('');
-    }, [])
+  React.useEffect(() => {
+    setValue('');
+  }, [])
 
-    return (
-        <div className={styles.searchOffer}>
-            <div className={styles.inputSearch}>
-                <input type="search" value={value} placeholder='Поиск'
-                       onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                           e && setValue(e.target.value);
-                       }}
-                       onFocus={() => {
-                           setToggleContainer(true);
-                       }}
-                       onBlur={() => {
-                           setToggleContainer(false);
-                       }}
-                />
-                <button
-                    className={`${styles.clear} ${value && value.length !== 0 ? styles.showClear : ''}`}
-                    onClick={() => {
-                        setValue('')
-                    }}>
-                    <span/>
-                </button>
-            </div>
-            <div className={`${styles.searchResult} ${toggleContainer ? styles.active : ''}`}>
-                {!loading ?
-                    products.length !== 0 ? products.map(({image, name, price, slug}, index: number) =>
-                        <SearchProduct key={`${name}_${index}`} image={image} name={name} price={price} slug={slug}/>
-                    ) : 'Ничего не найдено' : <Loader/>
-                }
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.searchOffer}>
+      <div className={styles.inputSearch}>
+        <input type="search" value={value} placeholder='Поиск'
+               onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                 e && setValue(e.target.value);
+               }}
+               onFocus={() => {
+                 setToggleContainer(true);
+               }}
+               onBlur={() => {
+                 setToggleContainer(false);
+               }}
+        />
+        <button
+          className={`${styles.clear} ${value && value.trim().length !== 0 ? styles.showClear : ''}`}
+          onClick={() => {
+            setValue('')
+          }}>
+          <span/>
+        </button>
+      </div>
+      <div className={`${styles.searchResult} ${toggleContainer ? styles.active : ''}`}>
+        {!loading ?
+          products.length !== 0 ? products.map(({image, name, price, slug}, index: number) =>
+            <SearchProduct key={`${name}_${index}`} image={image} name={name} price={price} slug={slug}/>
+          ) : 'Ничего не найдено' : <Loader/>
+        }
+      </div>
+    </div>
+  )
 }
 
 interface ISearchProduct {
-    image: {
-        mediaItemUrl: string
-    }
-    name: string
-    price: number
-    slug?: string
+  image: {
+    mediaItemUrl: string
+  }
+  name: string
+  price: number
+  slug?: string
 }
 
 function SearchProduct({image, name, price, slug}: ISearchProduct) {
-    return <Link href="/product/[slug]" as={`/product/${slug}/`}>
-        <a>
-            <div className={styles.productContainer}>
-                <div className={styles.image}>
-                    <Image src={image.mediaItemUrl} layout='fill' objectFit="contain"/>
-                </div>
-                <h2 className={styles.title}>{name}</h2>
-                <p className={styles.price}>{price} руб.</p>
-            </div>
-        </a>
-    </Link>
+  return <Link href="/product/[slug]" as={`/product/${slug}/`}>
+    <a>
+      <div className={styles.productContainer}>
+        <div className={styles.image}>
+          <Image src={image.mediaItemUrl} layout='fill' objectFit="contain"/>
+        </div>
+        <h2 className={styles.title}>{name}</h2>
+        <p className={styles.price}>{price} руб.</p>
+      </div>
+    </a>
+  </Link>
 }
