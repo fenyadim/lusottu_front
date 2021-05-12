d3235896627d3eccb84f4839811bab8be45c67a6
fenyadim <dimos9813@gmail.com>
2021-05-12 15:09:35 +0800

    Test

--- component/Product/SingleProduct/index.tsx
+++ component/Product/SingleProduct/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 import { ISingleProduct } from '../../../lib/types';
 
-import { Loader, AttributeWrapper } from '../..';
+import { Loader, Attribute } from '../..';
 
 import styles from './SingleProduct.module.scss';
 
@@ -20,38 +20,12 @@ const SingleProduct: React.FC<ISingleProduct> = ({ product, isLoading }) => {
         />
       </div>
       <div className={styles.infoSide}>
-        <AttributeWrapper value={name}>
-          <h1 className={styles.title}>{name}</h1>
-        </AttributeWrapper>
-        <AttributeWrapper value={brand.name}>
-          <h3 className={styles.brand}>
-            Бренд: <span>{brand.name}</span>
-          </h3>
-        </AttributeWrapper>
-        <AttributeWrapper value={desc}>
-          <p className={styles.desc}>
-            <span className={styles.itemTitle}>Описание: </span>
-            {desc}
-          </p>
-        </AttributeWrapper>
-        <AttributeWrapper value={notes}>
-          <p className={styles.notes}>
-            <span className={styles.itemTitle}>Ноты: </span>
-            {notes}
-          </p>
-        </AttributeWrapper>
-        <AttributeWrapper value={price}>
-          <h3 className={styles.price}>
-            Цена:
-            <span>{price} руб.</span>
-          </h3>
-        </AttributeWrapper>
-        <AttributeWrapper value={volume}>
-          <h3 className={styles.volume}>
-            Объем:
-            <span>{volume} мл.</span>
-          </h3>
-        </AttributeWrapper>
+        <h1 className={styles.title}>{name}</h1>
+        <Attribute name="Бренд" value={brand.name} />
+        <Attribute name="Описание" value={desc} />
+        <Attribute name="Ноты" value={notes} />
+        <Attribute name="Цена" value={price} addition="руб." />
+        <Attribute name="Объем" value={volume} addition="мл." />
       </div>
     </div>
   ) : (
